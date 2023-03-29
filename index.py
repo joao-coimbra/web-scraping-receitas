ef75cd995c3a857813bf59c5b7390c350b63930d
João Coimbra <joao.benatti.coimbra@gmail.com>
2023-03-29 13:16:11 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,34 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Fazer uma requisição para a página
-url = "https://www.receiteria.com.br/receita/macarrao-alho-e-oleo/"
-response = requests.get(url)
 
-# Criar o objeto BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
+def get_receita(receita):
+    # Fazer uma requisição para a página
+    url = f"https://www.receiteria.com.br/receita/{receita.replace(' ', '-')}/"
+    response = requests.get(url)
 
-# Buscar dados
-receita = {}
-receita['nome'] = soup.find('h1').text.strip()
-[receita['rendimento'], receita['tempo']] = [x.text.strip() for x in soup.find('div', {'class': 'info-recipe'}).find_all('span', limit=2)]
-receita['ingredientes'] = [x.text.strip() for x in soup.find("div", attrs={"class": "ingredientes"}).find_all("label")]
-receita['modo_preparo'] = [x.text.strip() for x in soup.find("ol", attrs={"class": "lista-preparo-1"}).find_all("span")]
-receita['dicas'] = [x.text.strip() for x in soup.find("div", attrs={"class": 'infosadicionais'}).find_all("p")]
+    # Criar o objeto BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Escrever no arquivo receita.md
-with open('receita.md', 'w', encoding='utf-8') as f:
-    f.write('# {}\n\n'.format(receita['nome']))
-    f.write('- {}\n'.format(receita['rendimento']))
-    f.write('- {}\n'.format(receita['tempo']))
+    receita = {}
 
-    f.write('\n## Ingredientes\n\n')
-    for x in receita['ingredientes']:
-        f.write(f'- {x}\n')
+    try:
+        # Buscar dados
+        receita['nome'] = soup.find('h1').text.strip()
+        [receita['rendimento'], receita['tempo']] = [x.text.strip() for x in soup.find('div', {'class': 'info-recipe'}).find_all('span', limit=2)]
+        receita['ingredientes'] = [x.text.strip() for x in soup.find("div", attrs={"class": "ingredientes"}).find_all("label")]
+        receita['modo_preparo'] = [x.text.strip() for x in soup.find("ol", attrs={"class": "lista-preparo-1"}).find_all("span")]
+        receita['dicas'] = [x.text.strip() for x in soup.find("div", attrs={"class": 'infosadicionais'}).find_all("p")]
+        
+        return receita
+    except Exception:
+        raise Exception('Possivelmente essa receita não está na base de dados, tente outra.')
+    
 
-    f.write('\n## Modo de preparo\n\n')
-    for index, x in enumerate(receita['modo_preparo']):
-        f.write(f'{index+1}. {x}\n')
+def write_receita(receita):
 
-    f.write('\n## Dicas\n\n')
-    f.write('\n'.join(receita['dicas']))
+    # Escrever no arquivo receita.md
+    with open('receita.md', 'w', encoding='utf-8') as f:
+        f.write('# {}\n\n'.format(receita['nome']))
+        f.write('- {}\n'.format(receita['rendimento']))
+        f.write('- {}\n'.format(receita['tempo']))
+
+        f.write('\n## Ingredientes\n\n')
+        for x in receita['ingredientes']:
+            f.write(f'- {x}\n')
+
+        f.write('\n## Modo de preparo\n\n')
+        for index, x in enumerate(receita['modo_preparo']):
+            f.write(f'{index+1}. {x}\n')
+
+        f.write('\n## Dicas\n\n')
+        f.write('\n'.join(receita['dicas']))
+
+    print('Arquivo salvo em /receita.md')
+
+
+if __name__ == '__main__':
+    receita = get_receita(input('Escolha uma receita: '))
+    write_receita(receita)
